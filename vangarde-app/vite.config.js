f18e15ec20dc6138879ed88e4fdbf4c4b7d930cf
stefanwerk <stefanweerk@gmail.com>
2025-10-21 11:10:12 +0200

    testen voor runnen

--- vangarde-app/vite.config.js
+++ vangarde-app/vite.config.js
@@ -4,17 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "./",
-  server: {
-    proxy: {
-      // Dev-only proxy: verwijst /api naar je lokale API-server
-      "/api": {
-        target: "http://127.0.0.1:4000", // pas poort aan als jouw API op andere poort draait
-        changeOrigin: true,
-        secure: false,
-        timeout: 10000,
-        proxyTimeout: 10000,
-      },
-    },
-  },
+  base: "", 
 });
